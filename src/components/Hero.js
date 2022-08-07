b4cc2254c46772795f9bb8558ca88a6fa551de97
archa-agrawal <archana.agrawal3@outlook.com>
2022-08-07 18:20:59 -0400

    formatted

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = () => (
+  <div>
+    <p>Hi, my name is</p>
+    <h2>Archana Agrawal,</h2>
+    <h3>I am a web developer</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ultrices dui sapien
+      eget mi proin sed libero. Suscipit adipiscing bibendum est ultricies
+      integer quis.
+    </p>
+  </div>
+);
+
+export default Hero;
