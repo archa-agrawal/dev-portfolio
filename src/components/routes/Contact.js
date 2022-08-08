7c01dfdb72eaa8a5dd5f64b5ae0aa67a5cb31814
archa-agrawal <archana.agrawal3@outlook.com>
2022-08-07 20:38:43 -0400

    Added basic layout styling

--- src/components/routes/Contact.js
+++ src/components/routes/Contact.js
@@ -3,6 +3,7 @@ import React from "react";
 const Contact = () => {
   return (
     <div style={{ backgroundColor: "green" }}>
+      <h3>Contact me</h3>
       <form>
         <label htmlFor={"name"}>Name</label>
         <input type={"text"} id={"name"} />
@@ -12,6 +13,7 @@ const Contact = () => {
         <input type={"text"} id={"message"} />
         <input type={"button"} value={"submit"} />
       </form>
+      <p>Or reach out on archana.agrawal3@outlook.com</p>
     </div>
   );
 };
