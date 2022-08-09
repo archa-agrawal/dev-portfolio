0a7417d8d74801db26df30761c53cf17ace8f21e
archa-agrawal <archana.agrawal3@outlook.com>
2022-08-08 21:36:28 -0400

    formatted pages

--- src/components/routes/Hero.js
+++ src/components/routes/Hero.js
@@ -1,16 +1,20 @@
 import React from "react";
 
+import "./hero.scss";
+
 const Hero = () => (
   <div style={{ backgroundColor: "yellow" }} id={"home"}>
-    <p>Hi, my name is</p>
-    <h2>Archana Agrawal,</h2>
-    <h3>I am a web developer</h3>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ultrices dui sapien
-      eget mi proin sed libero. Suscipit adipiscing bibendum est ultricies
-      integer quis.
-    </p>
+    <div className={"hero-container"}>
+      <p>Hi, my name is</p>
+      <h2>Archana Agrawal,</h2>
+      <h3>I am a web developer</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ultrices dui sapien
+        eget mi proin sed libero. Suscipit adipiscing bibendum est ultricies
+        integer quis.
+      </p>
+    </div>
   </div>
 );
 
--- src/components/routes/Intro.js
+++ src/components/routes/Intro.js
@@ -1,27 +1,31 @@
 import React from "react";
 
+import "./intro.scss";
+
 const Intro = () => (
   <div style={{ backgroundColor: "lightblue" }} id={"intro"}>
-    <h3>Intro</h3>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ultrices dui sapien
-      eget mi proin sed libero. Suscipit adipiscing bibendum est ultricies
-      integer quis. Tortor id aliquet lectus proin nibh nisl condimentum.
-    </p>
-    <p>
-      Egestas quis ipsum suspendisse ultrices gravida dictum. Orci nulla
-      pellentesque dignissim enim sit amet venenatis urna. Orci dapibus ultrices
-      in iaculis.
-    </p>
-    <h3>Tech I have worked with</h3>
-    <ul>
-      <li>JS</li>
-      <li>React</li>
-      <li>Node.js</li>
-      <li>Gatsby</li>
-      <li>Postgres</li>
-    </ul>
+    <div className={"intro-container"}>
+      <h3>Intro</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ultrices dui sapien
+        eget mi proin sed libero. Suscipit adipiscing bibendum est ultricies
+        integer quis. Tortor id aliquet lectus proin nibh nisl condimentum.
+      </p>
+      <p>
+        Egestas quis ipsum suspendisse ultrices gravida dictum. Orci nulla
+        pellentesque dignissim enim sit amet venenatis urna. Orci dapibus
+        ultrices in iaculis.
+      </p>
+      <h3>Tech I have worked with</h3>
+      <ul>
+        <li>JS</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>Gatsby</li>
+        <li>Postgres</li>
+      </ul>
+    </div>
   </div>
 );
 
