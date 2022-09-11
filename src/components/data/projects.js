d5f9fb2536177316f7342800827a2331694dce73
archa-agrawal <archana.agrawal3@outlook.com>
2022-09-11 11:13:42 -0400

    added mac-images

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -5,7 +5,7 @@ const projects = [
     name: "Mapi",
     description:
       "MAPI is the map based social media app. It lets users create and share maps with the world. Users can create a map with a title and description, pin different types of locations on the map, and can share it. \n        \nOther registered and non registered users can see the maps created by others. Maps can also be assigned themes for personalization.",
-    img: "./mapi.png",
+    img: "./mac-mapi.png",
     techs: [
       techs.node,
       techs.react,
@@ -31,7 +31,7 @@ const projects = [
     name: "Decision Maker",
     description:
       "Decision maker is an app that helps a group decide the best option by ranking the given choices. Users can create polls with as many choices they want. The app uses Mailgun API to mail the link of the poll to creators. They can vote and share the link of the polls with others. \nVoters can rank their choices by dragging and dropping. The creator of the poll can access the poll results using the result link at any time. This app uses Borda count ranking method to rank the choices.",
-    img: "./decisionMaker.png",
+    img: "./mac-dm.png",
     techs: [
       techs.node,
       techs.ejs,
@@ -52,7 +52,7 @@ const projects = [
     name: "Interview Scheduler",
     description:
       "This app helps a recruitment organization manage interview calendars for its employees and candidates. Users can book, edit, and delete their interviews.",
-    img: "./scheduler.png",
+    img: "./mac-scheduler.png",
     techs: [
       techs.node,
       techs.react,
