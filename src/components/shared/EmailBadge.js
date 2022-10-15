7ed2980e4e1320794596967bf61755f89a63e026
archa-agrawal <archana.agrawal3@outlook.com>
2022-10-15 00:39:09 -0400

    updated email address

--- src/components/shared/EmailBadge.js
+++ src/components/shared/EmailBadge.js
@@ -4,7 +4,7 @@ import "./emailBadge.scss";
 export default function EmailBadge() {
   return (
     <div className={"email-badge"}>
-      <a href={"mailto:archana.agrawal3@outlook.com"} className={"email"}>
+      <a href={"mailto:archana.agrawal604@gmail.com"} className={"email"}>
         archana.agrawal3@outlook.com
       </a>
     </div>
